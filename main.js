81169c9b1d4d9b5405b26c2e02ea5c84934c9674
Kyaw Zayar Win <25404467+starryskadi@users.noreply.github.com>
2018-10-27 21:17:42 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ items.forEach(function(item, index) {
             $("#page1").fadeIn(500);
             $("#page2").fadeOut(500);
             $("#page3").fadeOut(500);
+         
         } else if (index == 1) {
             items[0].classList.remove('active');
             items[2].classList.remove('active');
@@ -24,5 +25,24 @@ items.forEach(function(item, index) {
         }
     })
 })
+
+// add fixed class in top menu , extend margin-top 4em, and show/hide on screen size;
+function addFixedAndShow() {
+    if($(window).width()>768) {
+        $('.left.menu .item:not(.header)').show();
+        $('.ui.top.menu.inverted').addClass('fixed');
+        $('.main').css('margin-top','4em');
+    } else {
+        $('.left.menu .item:not(.header)').hide();
+    }
+}
+
+$(document).ready(function() {
+    $('#mobile-nav').click(function() {
+        $('.left.menu .item:not(.header)').toggle(500);
+    }); 
+    addFixedAndShow();
+    $(window).resize(addFixedAndShow)
+});
 
 
